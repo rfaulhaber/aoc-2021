8ab32ec0e7d9cab69914530c25a36498d769628f
Ryan Faulhaber <ryf@sent.as>
2021-11-29 10:51:37 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Advent of Code 2021 code environment.";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    # NOTE: this might only work in Linux!
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [ pkgs.bashInteractive ];
+          buildInputs = with pkgs; [
+            clang
+            racket
+            ghc
+            ocaml
+            gforth
+            clisp
+            elixir
+          ];
+        };
+      });
+}
