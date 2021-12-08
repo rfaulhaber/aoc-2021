bb86e9470657c5f2938dee4de28be21e38399a7c
Ryan Faulhaber <ryf@sent.as>
2021-12-07 20:06:25 -0500

    incomplete julia solution

--- 4/julia/solution.jl
+++ 4/julia/solution.jl
@@ -1,12 +1,18 @@
 sample = open("../sample.txt", "r")
 input = open("../input.txt", "r")
 
+completeMatrix = [1  2  3  4  5
+                  6  7  8  9  10
+                  11 12 13 14 15
+                  16 17 18 19 20
+                  21 22 23 24 25]
+
 struct Board
-    numbers::Array{Int32}
+    numbers::Set{String}
 end
 
 function readInput(file)
-    numbers = split(readline(file), ",")
+    numbers = map(s->String(s), split(readline(file), ","))
     boards = []
 
     currentBoard = []
@@ -27,9 +33,40 @@ function readInput(file)
     currentBoard = filter(s->!isempty(s), currentBoard)
     push!(boards, currentBoard)
 
-    return (numbers, boards)
+    boardList = map(b->Board(Set(b)), boards)
+
+    return (numbers, boardList)
 end
 
+function boardMatches(numbers, board::Board)
+    matches = []
+    n = collect(board.numbers)
+    for number in numbers
+        push!(matches, findfirst(v->v == number, n))
+    end
+
+    matches
+end
+
+function hasBingo(numbers, matches)
+    sortedMatches = sort(matches)
+
+    for row in [1:5]
+
+    end
+end
+
+function boardSolvedAt(numbers, board::Board)
+
+end
+
+function solvePart1(numbers, boards::Array{Board})
+
+end
+
+
 numbers, boards = readInput(sample)
-println("numbers: ", numbers)
-println("boards: ", boards)
+
+println(numbers)
+println(boards[1])
+println(boardMatches(numbers, boards[1]))
